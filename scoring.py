1b953680566f8be0ca7d0b5acac39c3314fb4451
astbi <168831619+astbi@users.noreply.github.com>
2024-05-14 11:00:05 +0000

    started playing with scoring table

--- scoring.py
+++ scoring.py
@@ -1,22 +1,30 @@
 import pandas as pd
-from read_tab_files import TabFileReader
 
 class Scoring:
-    def __init__(self, cognacy_matrix, goldfile):
+    def __init__(self, cognacy_matrix, goldfile, threshold=4):
         self.goldfile = goldfile
         self.cognacy_matrix = cognacy_matrix
+        self.threshold = threshold
 
-    def pairwise_cognacy(self):
+    def accuracy(self):
+        for i in self.cognacy_matrix:
+            print(i)
+    
+    def gold_table(self):
         for row in range(len(self.goldfile)):
             for col in range(len(self.goldfile.loc[row])-1):
                 if row != 0 and col != 0:
-                    #print(self.goldfile.loc[row, col])
-                    if self.goldfile.loc[row, col] == self.goldfile.loc[row, col+1]:
-                        # How to save the cognacy data?
+                    print(self.goldfile.loc[row, col])
+                    #if self.goldfile.loc[row, col] == self.goldfile.loc[row, col+1]:
+                    # How to save the cognacy data?
+    
+
 
 
 
 if __name__ == '__main__':
+    from read_tab_files import TabFileReader
+    from main import score_list
     ie_cognacy = TabFileReader.tab_reader("chl2024_iedata/chl2023_iedata_cognacy.tab")
-    scoring_ie = Scoring(ie_cognacy, ie_cognacy)
-    scoring_ie.accuracy(scoring_ie)
+    ie_scoring = Scoring(ie_cognacy, score_list)
+    ie_scoring.accuracy()

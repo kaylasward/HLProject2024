0ff0d5d3fe133606695dd4dbca121baa0fc89b9b
astbi <168831619+astbi@users.noreply.github.com>
2024-05-14 08:55:13 +0000

    deleted some playing around code

--- read_tab_files.py
+++ read_tab_files.py
@@ -17,8 +17,6 @@ eau_forms = tab_reader('chl2024_eastern-austronesiandata/chl2023_eastern-austron
 ie_cognacy = tab_reader('chl2024_iedata/chl2023_iedata_cognacy.tab')
 ie_forms = tab_reader('chl2024_iedata/chl2023_iedata_forms.tab')
 
-from old_code_notes.AutoCorrect import AutoCorrect
-
 if __name__ == "__main__":
     print(barb_cognacy.loc[[0,1,2]])
     print()

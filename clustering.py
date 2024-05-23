1b17013804b1c9fe21704ebc195acf40d24e9c6a
astbi <168831619+astbi@users.noreply.github.com>
2024-05-23 11:38:53 +0000

    Family-neutral code for removing gaps (doesn't work yet)

--- clustering.py
+++ clustering.py
@@ -34,6 +34,27 @@ def upgma2taxa_dict(calculated_dict):
             output_clusters[lang] = set(str(clus))
     return output_clusters
 
+def remove_gaps(cognates, gold_cognates, taxa):
+    """ Removes the cognate from both lists where either
+     list has a gap. Should work for all families """
+    # Save the indices where at least one of the files have a gap
+    assert len(cognates) == len(gold_cognates)
+    empty_indices = []
+    print("Lists before gaps are removed:",cognates, gold_cognates, taxa)
+    for i in range(len(cognates)):
+        if cognates[i] == "":
+            empty_indices.append(i)
+        elif gold_cognates[i] == "":
+            empty_indices.append(i)
+    # Pop the items last to first
+    for n in reversed(empty_indices):
+        cognates.pop(n)
+        gold_cognates.pop(n)
+        taxa.pop(n)
+    print("Lists after gaps are removed:",cognates, gold_cognates, taxa)
+    assert len(cognates) == len(gold_cognates) == len(taxa)
+    return cognates, gold_cognates, taxa
+
 def prepare_comparison(threshold:float, gold_row:list, cognates:list, taxa:list)->tuple:
     distance_matrix = get_distancematrix(cognates)
     cluster_dict = lingpy.flat_upgma(threshold,distance_matrix,taxa)
@@ -41,27 +62,23 @@ def prepare_comparison(threshold:float, gold_row:list, cognates:list, taxa:list)
     gold_taxadict = gold_clust2taxa_dict(taxa, gold_row)
     return taxa_dict, gold_taxadict
 
-def main(language_fam, fam_gold, threshold=4, remove_gaps=True):
+def main(language_fam, fam_gold, threshold=4):
     precision = 0
     recall = 0
     fscore = 0
-    taxa = list(language_fam.iloc[0][1:])
+    full_taxa = list(language_fam.iloc[0][1:])
     for i in range(1, len(language_fam)):
         cognates = list(language_fam.iloc[i][1:])
-        gold_row = list(fam_gold.iloc[i][1:])
-        if remove_gaps == True:
-            if "" in gold_row:
-                gold_row.remove("")
-            if "" in cognates:
-                cognates.remove("")
-        taxa_dict, gold_taxadict = prepare_comparison(threshold, gold_row, cognates, taxa)
-        precision += bcubed.precision(taxa_dict, gold_taxadict)
+        gold_cognates = list(fam_gold.iloc[i][1:])
+        cognates, gold_cognates, cut_taxa = remove_gaps(cognates, gold_cognates, full_taxa) # Remove gaps
+        taxa_dict, gold_taxadict = prepare_comparison(threshold, gold_cognates, cognates, cut_taxa)
+        precision += bcubed.precision(taxa_dict, gold_taxadict) # Here
         recall += bcubed.recall(taxa_dict, gold_taxadict)
     n_rows = len(language_fam)-1
     precision /= n_rows
     recall /= n_rows
     fscore = bcubed.fscore(precision, recall)
-    return precision, recall, fscore
+    return round(precision,2), round(recall,2), round(fscore,2)
 
 #####
 
@@ -80,16 +97,18 @@ ie_forms = TabFileReader.tab_reader("chl2024_iedata/chl2023_iedata_forms.tab")
 
 barb_scores_4 = main(barb_forms, barb_cognacy)
 barb_scores_2 = main(barb_forms, barb_cognacy, threshold=2)
-#eau_scores_4 = main(eau_forms, eau_cognacy)
-#eau_scores_2 = main(eau_forms, eau_cognacy, threshold=2)
+eau_scores_4 = main(eau_forms, eau_cognacy)
+eau_scores_2 = main(eau_forms, eau_cognacy, threshold=2)
 ie_scores_4 = main(ie_forms, ie_cognacy)
 ie_scores_2 = main(ie_forms, ie_cognacy, threshold=2)
 
-print("Family        Threshold  Precision     Recall           F-score")
-print("Barbacoan     2  ", barb_scores_2)
-print("Barbacoan     4  ", barb_scores_4)
-print("Indo-European 2  ", ie_scores_2)
-print("Indo-European 4  ", ie_scores_4)
+print("Family       Threshold Precision Recall F-score")
+print("Barbacoan            2  ", barb_scores_2)
+print("Barbacoan            4  ", barb_scores_4)
+print("Eastern Austronesian 2  ", eau_scores_2)
+print("Eastern Austronesian 4  ", eau_scores_4)
+print("Indo-European        2  ", ie_scores_2)
+print("Indo-European        4  ", ie_scores_4)
 
 # Trying Eastern Austronesian data gives weird error - why?
 # Scores are the same with different thresholds + why?

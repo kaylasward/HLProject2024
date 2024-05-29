d27ec6775ea78b79e008e9a784ba0950e4830ed6
Peter Cady <pccady@gmail.com>
2024-05-29 21:55:32 +0200

    updated plots

--- plot_all_metrics.py
+++ plot_all_metrics.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from clustering import Clustering
 from read_tab_files import TabFileReader
 
+from trimming import trim_all
+
 barb_cognacy = TabFileReader.tab_reader(
     "chl2024_barbacoandata/chl2023_barbacoan_cognacy.tab"
 )
@@ -29,6 +31,7 @@ form_data = {
     "ie": (ie_forms, ie_cognacy),
 }
 
+
 ## Add new metric #TODO
 distance_metrics = [
     "levenshtein_nltk",
@@ -44,10 +47,25 @@ optimal_thresholds = {
     "ie": 0.8,
 }
 
+optimal_trim_thresholds = {
+    "barb": {"core": (0.0,0.8),
+             "gap": (0.7,0.7)},
+    "eau": {"core": (0.1,0.4),
+            "gap": (1.0,0.7)},
+    "ie": {"core": (0.0,0.7),
+           "gap": (0.8,0.8)},
+}
 
 precision_df = pd.DataFrame(columns=distance_metrics, index=form_data.keys())
 recall_df = pd.DataFrame(columns=distance_metrics, index=form_data.keys())
 f1_df = pd.DataFrame(columns=distance_metrics, index=form_data.keys())
+
+core_prec = []
+core_recall = []
+core_f1 = []
+gap_prec = []
+gap_recall = []
+gap_f1 = []
 
 for form_name, (forms, cognacy) in form_data.items():
     for distance_metric in distance_metrics:
@@ -64,6 +82,49 @@ for form_name, (forms, cognacy) in form_data.items():
         precision_df.loc[form_name, distance_metric] = precision
         recall_df.loc[form_name, distance_metric] = recall
         f1_df.loc[form_name, distance_metric] = f1
+        
+    #gap-oriented
+    gap_form = trim_all(
+        forms,
+        threshold=optimal_trim_thresholds[form_name]["gap"][0],
+        strategy='gap-oriented'
+    )
+    
+    clusters = Clustering(
+        gap_form,
+        cognacy,
+        threshold=optimal_trim_thresholds[form_name]["gap"][1],
+    )
+
+    precision, recall, f1 = clusters.score()
+    gap_prec.append(precision)
+    gap_recall.append(recall)
+    gap_f1.append(f1)
+    
+    #core-oriented
+    core_form = trim_all(
+        forms,
+        threshold=optimal_trim_thresholds[form_name]["core"][0]
+    )
+    clusters = Clustering(
+        core_form,
+        cognacy,
+        threshold=optimal_trim_thresholds[form_name]["core"][1],
+    )
+
+    precision, recall, f1 = clusters.score()
+    core_prec.append(precision)
+    core_recall.append(recall)
+    core_f1.append(f1)
+    
+precision_df["gap_trim"] = gap_prec
+precision_df["core_trim"] = core_prec
+
+recall_df["gap_trim"] = gap_recall
+recall_df["core_trim"] = core_recall
+
+f1_df["gap_trim"] = gap_f1
+f1_df["core_trim"] = core_f1
 
 ## Add new label #TODO
 legend_labels = [
@@ -72,16 +133,18 @@ legend_labels = [
     "Levenshtein Phonetic",
     "Global Alignment",
     "Local Alignment",
+    "NLTK w/ Gap-trim",
+    "NLTK w/ Core-trim"
 ]
 
 x_labels = [
-    "Barbacoan\n(threshold: 0.8)",
-    "Eastern Austronesian\n(threshold:0.6)",
-    "Indo-European\n(threshold:0.8)",
+    "Barbacoan\n(cluster threshold: 0.8)\n(gap t.hold: 0.7, cluster t.hold:0.7)\n(core t.hold: 0.0, cluster t.hold:0.8)",
+    "Eastern Austronesian\n(cluster threshold:0.6)\n(gap t.hold: 1.0, cluster t.hold:0.7)\n(core t.hold: 0.1, cluster t.hold:0.4)",
+    "Indo-European\n(cluster threshold:0.8)\n(gap t.hold: 0.8, cluster t.hold:0.8)\n(core t.hold: 0.0, cluster t.hold:0.7)",
 ]
 
 ## Add any new color #TODO
-color_palette = ["#4daf4a", "#377eb8", "#ff7f00", "#984ea3", "#e41a1c"]
+color_palette = ["#4daf4a", "#377eb8", "#ff7f00", "#984ea3", "#e41a1c", "0.5", "0"]
 
 ax = precision_df.plot(kind="bar", figsize=(12, 5), width=0.7, color=color_palette)
 plt.title("Precision Scores")
